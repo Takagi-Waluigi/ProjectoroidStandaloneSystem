7490344bd617db941986f98541d7a0ea7d4d7f68
Kenta Takagi <tokage.kentacky420@gmail.com>
2024-11-14 18:29:26 +0900

    Add Projection Blind Function

--- Assets/Scripts/OscSystem/ReceiveTransform.cs
+++ Assets/Scripts/OscSystem/ReceiveTransform.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class ReceiveTransform : MonoBehaviour
 {
     [SerializeField] float deactivateTime = 2f;
+    [SerializeField] bool isForScreenBlind = false;
     public bool isReciveingInfo = false;
     bool _isReciveingInfo = false;
     float falseTime = 0;
+    float fixedHeight = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fixedHeight = this.transform.position.y;
     }
 
     // Update is called once per frame
@@ -33,13 +35,23 @@ public class ReceiveTransform : MonoBehaviour
 
     public void RecivePosition(Vector3 position)
     {
+        if(isForScreenBlind) position.y = fixedHeight;
+
         this.gameObject.transform.position = position;
         _isReciveingInfo = true;
     } 
 
     public void RecieveRotation(Vector3 eulerAngles)
     {
-        this.gameObject.transform.rotation = Quaternion.Euler(eulerAngles);
+        if(isForScreenBlind)
+        {
+            this.gameObject.transform.rotation = Quaternion.Euler(0f, eulerAngles.y, 0f);
+        }
+        else
+        {
+            this.gameObject.transform.rotation = Quaternion.Euler(eulerAngles);
+        }
+        
         _isReciveingInfo = true;
     }
 }
